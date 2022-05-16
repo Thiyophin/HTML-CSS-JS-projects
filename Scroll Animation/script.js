a5460376622f8253951bb9710883dfc6ab3fd655
Thiyophin Johnson <thiyophin@gmail.com>
2022-05-16 13:04:37 +0530

    scroll animation fully completed

--- Scroll Animation/script.js
+++ Scroll Animation/script.js
@@ -2,6 +2,7 @@ const boxes = document.querySelectorAll('.box')
 
 window.addEventListener('scroll',checkboxes);
 
+checkboxes()
 function checkboxes(){
   const triggerBottom = window.innerHeight / 5*4;
   boxes.forEach(box => {
